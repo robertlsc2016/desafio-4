f8742dc00722f998b766362922f521e0e3cea9c9
RobertUmbler <79148833+RobertUmbler@users.noreply.github.com>
2021-02-17 14:50:36 -0400

    para o github

--- app4.js
+++ app4.js
@@ -6,7 +6,7 @@ const dontenv = require('dotenv').config()
 
 var connection = mysql.createConnection ({
     host     : process.env.DB_HOST,
-    port    : process.env.DB_PORTA, //SEM DEFINIR A PORTA DAVA O ERRO 'connect ETIMEDOUT'
+    port     : process.env.DB_PORTA, //SEM DEFINIR A PORTA DAVA O ERRO 'connect ETIMEDOUT'
     user     : process.env.DB_USER,
     password : process.env.DB_PASSWORD,
     database : process.env.DB_DATABASE
